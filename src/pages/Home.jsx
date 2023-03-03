99aebca25496e2c0fd3ff648ae115253219c800e
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-03-03 23:22:11 +0530

    home page content update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import HeroSection from "../components/HeroSection";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <HeroSection />
     </>
   );
 };
