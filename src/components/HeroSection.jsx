f3464ebf2f895fffe24e5bc1b62fe44de50b7931
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-03-04 12:53:02 +0530

    component update

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import ButtonFour from "./ButtonFour";
 import ButtonThree from "./ButtonThree";
 import ButtonTwo from "./ButtonTwo";
 const HeroSection = () => {
@@ -22,15 +23,32 @@ const HeroSection = () => {
           <p>Virtual reality, or VR, is talking off in education with an</p>
           <p>increasing number of schools adopting the technology,</p>
         </div>
+        <div className="left-btn">
+          <ButtonFour name="Get Started"></ButtonFour>
+        </div>
+      </div>
+      <div className="right-section">
+        <img src="./images/hero-img.png" alt="heroimg" />
       </div>
     </Wrapper>
   );
 };
 const Wrapper = styled.div`
+  display: flex;
+  /* align-items: center; */
+  justify-content: space-between;
+  margin-top: 84px;
+  padding-left: 54px;
+  /* margin-right: 54px; */
+  overflow-x: hidden;
+  /* border: 1px solid red; */
   .head-section {
     font-size: 96px;
     width: 814px;
+    padding-top: 20px;
     line-height: 110px;
+    /* padding-left: 35px; */
+    margin-bottom: 49px;
     /* border: 1px solid red; */
     .head-info-one {
       display: flex;
@@ -48,8 +66,15 @@ const Wrapper = styled.div`
       gap: 1rem;
     }
   }
-  .sub-info p {
-    font-size: 24px;
+  .sub-info {
+    p {
+      font-size: 24px;
+      line-height: 43px;
+    }
+    margin-bottom: 47px;
+  }
+  .right-section {
+    /* margin-top: 10px; */
   }
 `;
 export default HeroSection;
