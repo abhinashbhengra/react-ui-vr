cf782975970537dcf8d810d30d64a2585fc8287b
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-03-04 14:52:20 +0530

    component added

--- src/components/ButtonTwo.jsx
+++ src/components/ButtonTwo.jsx
@@ -4,31 +4,22 @@ import styled from "styled-components";
 const ButtonTwo = () => {
   return (
     <Wrapper>
+      <div className="inner"></div>
       <div className="main">
         <img src="./images/vrwmn.png" alt="vr-logo" />
       </div>
-      <div className="inner"></div>
     </Wrapper>
   );
 };
 const Wrapper = styled.div`
-  position: relative;
   width: 205px;
   height: 85px;
+  position: relative;
   img {
     /* display: none; */
+    /* / */
     width: 181px;
     height: 120px;
-  }
-  .main {
-    background-color: #fab246;
-    border-radius: 61px;
-    width: 205px;
-    height: 85px;
-    border: 2px solid black;
-    display: flex;
-    align-items: flex-end;
-    justify-content: center;
   }
   .inner {
     width: 100%;
@@ -38,7 +29,18 @@ const Wrapper = styled.div`
     top: 4.56px;
     left: -6.87px;
     border-radius: 61px;
-    z-index: -1;
+  }
+  .main {
+    background-color: #fab246;
+    position: relative;
+    border-radius: 61px;
+    width: 205px;
+    height: 85px;
+    border: 2px solid black;
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+    z-index: 8881;
   }
 `;
 export default ButtonTwo;
