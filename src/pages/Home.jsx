8c94c2292176bfc427a593f82dbb750d3cbdc5ab
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-03-06 21:21:49 +0530

    component update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HeroSection from "../components/HeroSection";
 import Navbar from "../components/Navbar";
+import Section10 from "../components/Section10";
 import Section2 from "../components/Section2";
 import Section3 from "../components/Section3";
 import Section4 from "../components/Section4";
@@ -23,6 +24,7 @@ const Home = () => {
       <Section7 />
       <Section8 />
       <Section9 />
+      <Section10 />
     </>
   );
 };
