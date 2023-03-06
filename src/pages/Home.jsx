f1b8c66b97501b83da9daaa03d6770a0002e0b9d
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-03-06 18:35:15 +0530

    component update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import Section3 from "../components/Section3";
 import Section4 from "../components/Section4";
 import Section5 from "../components/Section5";
 import Section6 from "../components/Section6";
+import Section7 from "../components/Section7";
 
 const Home = () => {
   return (
@@ -17,6 +18,7 @@ const Home = () => {
       <Section4 />
       <Section5 />
       <Section6 />
+      <Section7 />
     </>
   );
 };
